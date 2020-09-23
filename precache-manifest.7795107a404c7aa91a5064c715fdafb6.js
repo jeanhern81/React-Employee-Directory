efb0b9f3192eff84905c0dc86b0b391c8ee040d3
jeanhern81 <jeanhern81@gmail.com>
2020-09-22 18:22:43 -0700

    Updates

---
+++ precache-manifest.7795107a404c7aa91a5064c715fdafb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11949a60763dfd76d8100e6a1c6f147",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "dc8b408a07e556615a94",
+    "url": "/React-Employee-Directory/static/css/main.7d180acf.chunk.css"
+  },
+  {
+    "revision": "2e33947f743af70ba74f",
+    "url": "/React-Employee-Directory/static/js/2.f49a0791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.f49a0791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8b408a07e556615a94",
+    "url": "/React-Employee-Directory/static/js/main.0d92c98c.chunk.js"
+  },
+  {
+    "revision": "7a3b04eb39eeeb27bc59",
+    "url": "/React-Employee-Directory/static/js/runtime-main.a0389cb5.js"
+  }
+]);
